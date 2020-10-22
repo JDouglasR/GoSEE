afed33d6a984ac4c0116ad5f929f4a7147dd5691
Justin Roose <justin.d.roose@gmail.com>
2020-10-21 21:28:20 -0400

    changed getPosts in /utils/API.js -- set up placeholder for api call to feed posts in /components/Feed.js

--- client/src/components/Feed/index.js
+++ client/src/components/Feed/index.js
@@ -1,26 +1,62 @@
 import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import API from "../../utils/API";
 
-let itemsArr =[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+let itemsArr = [
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+];
+
 const style = {
-    height: 30,
-    border: "1px solid green",
-    margin: 6,
-    padding: 8
-  };
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
+};
 
 function Feed() {
   const [items, setItems] = useState(itemsArr);
+  const [page, setPage] = useState(1);
 
   function fetchMoreData() {
     // a fake async api call like which sends
     // 20 more records in 1.5 secs
+    API.getPosts(setPage(page + 1));
+
     setTimeout(() => {
-      setItems(
-        items.concat(Array.from({ length: 20 }))
-      );
+      setItems(items.concat(Array.from({ length: 20 })));
     }, 1500);
-  };
+  }
 
   return (
     <React.Fragment>
@@ -31,6 +67,11 @@ function Feed() {
         next={fetchMoreData}
         hasMore={true}
         loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
         // below props only if you need pull down functionality
         // refreshFunction={refresh}
         // pullDownToRefresh
@@ -42,11 +83,11 @@ function Feed() {
         //   <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
         // }
       >
-          {items.map((i, index) => (
-            <div style={style} key={index}>
-              div - #{i}
-            </div>
-          ))}
+        {items.map((i, index) => (
+          <div style={style} key={index}>
+            div - #{i}
+          </div>
+        ))}
       </InfiniteScroll>
     </React.Fragment>
   );
