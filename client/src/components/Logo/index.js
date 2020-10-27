4e7bb0220503aa98896705bb1bad0690e2437f08
Michael Gragg <contact@michaeldgragg.com>
2020-10-26 22:08:20 -0400

    deleting logo styles

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./style.css";
 
-
 function Logo() {
   return (
     <React.Fragment>
-      <a href="" class="logo"><span>GO</span>SEE</a>
+      <span style={{fontFamily: "Roboto, sans-serif", fontWeight: "900",  fontSize: "40px", color: "white", padding: "20px", marginTop: "40px", marginLeft: "20px"}} ><span style={{color: "#f7ee24"}}>GO</span>SEE</span>
     </React.Fragment>
   );
 }
