3175ac7451a57fd779ed51d65a8e16dcac380877
Michael Gragg <contact@michaeldgragg.com>
2020-10-26 20:49:54 -0400

    Added TabPanel

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Navigation from "../Navigation";
-import "style.css";
+import "./style.css";
 
 function Header() {
   return (
     <React.Fragment>
-      <Navigation />
-      <h2>This is the Header</h2>
+      
     </React.Fragment>
   );
 }
