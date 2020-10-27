992075416a2575243b9da731fafd4e42e2729837
Josh Panakkal <josh_panakkal3122@hotmail.com>
2020-10-26 21:51:46 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,11 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-<<<<<<< HEAD
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-=======
+
 // // If no API routes are hit, send the React app
 // router.use(function(req, res) {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 // });
->>>>>>> 5f04adbb4417fe2a75f5df61b37e206dff983848
+
 
 module.exports = router;
