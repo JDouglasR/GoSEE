d7dec29b9304ed01e73d654c160c4155ad826e27
Michael Gragg <contact@michaeldgragg.com>
2020-10-28 00:00:53 -0400

    added styles to home page

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { Grid, Avatar, TextField, Button  } from '@material-ui/core';
 import "./style.css";
 
-function Navigation() {
+function Post() {
   return (
     <React.Fragment>
-      <h1>This is the Navigation</h1>
+       <Grid className="container" container direction="row" justify="center" alignItems="center" spacing={2}>
+        
+        <Grid item sm={1}>
+          <Avatar className="avatar" src="/broken-image.jpg" />
+        </Grid>
+
+        <Grid item sm={6}>
+        <TextField className="mainField" label="Share plans for any future trips" variant="outlined" />
+        <TextField className="locationField" label="#Location" variant="outlined" />
+        {/* <TextField className="mainField" label="Share reviews/plans of taken or future trips" />
+          <TextField className="locationField" label="#Location" /> */}
+          
+          <Button className="postButton" variant="contained">Post</Button>
+        </Grid>
+
+      </Grid>
     </React.Fragment>
   );
 }
 
-export default Navigation;
+export default Post;
+
+
