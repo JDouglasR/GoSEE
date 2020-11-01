f813c28c9a5f6935b138c283fb56292de0fc2df7
New Dad <josh_panakkal3122@hotmail.com>
2020-10-31 20:50:45 -0400

    recover cityName variable

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -15,10 +15,30 @@ module.exports = {
 
   // Get all posts with city name
   findById: function(req, res) {
-    const cityName = req.params.city;
-    db.Posts.find({ hashtag: cityName})
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.json(err));    
+    const cityName = req.params.city;    
+    db.Users.aggregate([
+          {
+            $lookup:
+            {
+                from: "posts",
+                localField: "posts",
+                foreignField: "_id",
+                as: "posts"
+            }
+          },
+          {
+            $unwind: "$posts"
+          }
+      ])
+      .then(dbUsers => {
+        var x = dbUsers.filter(city => {
+          return city.posts.hashtag.toLowerCase() === cityName.toLowerCase();
+        })
+            res.json(x);        
+          })   
+        .catch(err => {
+          res.json(err);
+        });
   },
 
   // Save user posts
@@ -34,50 +54,6 @@ module.exports = {
   }
 }
 
-//Post request to add posts to database and update user collection
-// app.post("/submit", ({body}, res) => {
-//   db.Posts.create(body)
-//     .then(({_id}) => db.Users.findOneAndUpdate({_id: userId}, { $push: { posts: _id } }, { new: true }))
-//     .then(dbUsers => {
-//       res.json(dbUsers);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-
-/* This variable is used to store the users' MongoDB id which will be used   
-when adding posts*/
-// var userId;
-
-/* Post request uses Project 2 form data to add user to database. Also updates 
-userId variable with id*/
-
-// app.post("/api/signup", (req, res) => {
-//   db.Users.create(req.body)
-//     .then(dbUsers => {
-//       console.log(dbUsers._id);
-//       userId = dbUsers._id;
-//     })
-//     .catch(({message}) => {
-//       console.log(message);
-//     });
-// });
-
-
-// 
-
-// Get all posts for user
-// app.get("/posts", (req, res) => {
-//   db.Posts.find()
-//     .then(dbPost => {
-//       res.json(dbPost);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// })
 
 // Route to get all posts using a city variable (ex/ cleveland)
 // app.get("/posts/:city", (req, res) => {
@@ -107,18 +83,6 @@ userId variable with id*/
 //   });
 // });
 
-
-// Get user
-// app.get("/user", (req, res) => {
-//   db.Users.find({})
-//     .then(dbUsers => {
-//       res.json(dbUsers);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
 // Get all users with their posts
 // app.get("/populated", (req, res) => {
 //   db.Users.aggregate([
@@ -144,76 +108,4 @@ userId variable with id*/
 //     });
 // });
 
-// Get all posts for user
-// app.get("/posts", (req, res) => {
-//   db.Posts.find()
-//     .then(dbPost => {
-//       res.json(dbPost);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// })
 
-// Route to get all posts using a city variable (ex/ cleveland)
-// app.get("/posts/:city", (req, res) => {
-//   const cityName = req.params.city;
-//   db.Users.aggregate([
-//     {
-//       $lookup:
-//       {
-//           from: "posts",
-//           localField: "posts",
-//           foreignField: "_id",
-//           as: "posts"
-//       }
-//     },
-//     {
-//       $unwind: "$posts"
-//     }
-// ])
-// .then(dbUsers => {
-//   var x = dbUsers.filter(city => {
-//     return city.posts.hashtag.toLowerCase() === cityName.toLowerCase();
-//   })
-//       res.json(x);
-//     })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
-
-// Get user
-// app.get("/user", (req, res) => {
-//   db.Users.find({})
-//     .then(dbUsers => {
-//       res.json(dbUsers);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// Get all users with their posts
-// app.get("/populated", (req, res) => {
-//   db.Users.aggregate([
-//       {
-//         $lookup:
-//         {
-//         from: "posts",
-//         localField: "posts",
-//         foreignField: "_id",
-//         as: "posts"
-//         }
-//       },
-//       {
-//         $unwind: "$posts"
-//       }
-//   ])
-//   .then(dbUsers => {
-//       res.json(dbUsers);
-//       })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
