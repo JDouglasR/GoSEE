2e10f050f65c97914491348a8f420f2a24be4ff1
New Dad <josh_panakkal3122@hotmail.com>
2020-11-04 19:59:37 -0500

    fixed posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -57,20 +57,11 @@ module.exports = {
   },
 
   // Save user posts
-  savePost: function({body}, res) {
-    console.log(body);
-    db.Posts.create(body)
-    .then(({_id}) => db.Users.findOneAndUpdate({_id: body.id}, { $push: { posts: _id } }, { new: true }))
-    .then(dbUsers => {
-      res.json(dbUsers);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-    // db.Posts.create(body)
-    // .then(dbPost => {
-    //   res.json(dbPost)
-    // })
-    // .catch(err => res.json(err));
+  savePost: function(req, res) {
+    console.log(req.body);
+    db.Posts.create(req.body)
+    .then(({_id}) => db.Users.findOneAndUpdate({_id: req.body.id}, { $push: { posts: _id } }, { new: true }))
+    .then(() => module.exports.findAll(req, res))
+    
   }
 };
